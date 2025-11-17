f250979737eeda10aea1e76760c5d7de0bb996b2
DrinkWaterAndBeHappy <s4mur41.95@mail.ru>
2025-11-17 13:23:33 +0300

    Исправленный вариант 2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,14 +1,15 @@
 package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import java.sql.SQLException;
+
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
         UserDao userDao = new UserDaoJDBCImpl();
-        UserServiceImpl userService = new UserServiceImpl(userDao);
+        UserService userService = new UserServiceImpl(userDao);
 
         userService.createUsersTable();
 
