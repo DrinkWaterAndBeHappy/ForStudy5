5fc2af06d257d3eefbde4757f851d2bfa14a7e31
DrinkWaterAndBeHappy <s4mur41.95@mail.ru>
2025-11-18 22:13:58 +0300

    Исправленный вариант 2

--- src/main/java/jm/task/core/jdbc/service/UserService.java
+++ src/main/java/jm/task/core/jdbc/service/UserService.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public interface UserService {
 
-
     void createUsersTable();
 
     void dropUsersTable();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+
 
 
 public class Util {
@@ -10,6 +19,7 @@ public class Util {
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "0n1oh";
     private static final String URL = "jdbc:mysql://localhost:3306/training";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
 
@@ -26,6 +36,41 @@ public class Util {
             throw new RuntimeException(e);
         }
     }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/study");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "0n1oh");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                sessionFactory = configuration.buildSessionFactory();
+                System.out.println("Сессия создана успешно");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static void closing() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+            System.out.println("Сессия закрыта");
+        }
+    }
 }
+
 
 
